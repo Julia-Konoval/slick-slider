561a361bcf71e4d0fdd69771643fd898afd46186
Julia <yulyakonoval1998@gmail.com>
2021-02-10 19:46:35 +0200

    slider layput created

--- slider.js
+++ slider.js
@@ -4,13 +4,26 @@ $(document).ready(function () {
     $(".nav ui").toggleClass("showing");
   });
 
+  $(".slider-nav-thumbnails").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    appendArrows: $('.your-class-arrow'),
+            prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i> Туда</button>',
+            nextArrow: '<button id="next" type="button" class="btn btn-juliet">Сюда <i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+    centerMode: true,
+    focusOnSelect: true,
+    infinite: false,
+  });
+
   $(".slider-for").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     // autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 6000,
   });
 });
+
 // $(function () {
 //   $(".slider-for").each(function (num, elem) {
 //     elem = $(elem);

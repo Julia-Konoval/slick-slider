46303fa3efe8fa5482047d5423471073fa35b71c
Julia <yulyakonoval1998@gmail.com>
2021-02-12 20:35:50 +0200

    paging counting was styled, font family changed

--- slider.js
+++ slider.js
@@ -5,13 +5,20 @@ $(document).ready(function () {
   });
   let $status = $(".pagingInfo");
   let $slickElement = $(".video-slider");
+  let $slickPrev = $(".slick-prev");
+  let $slickNext = $(".slick-next");
 
   $slickElement.on(
     "init reInit afterChange",
     function (event, slick, currentSlide, nextSlide) {
       //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
       let i = (currentSlide ? currentSlide : 0) + 1;
-
+      // if (i >= 4) {
+      //   $slickNext.css("display", "none");
+      // }
+      // if (i < 1) {
+      //   $slickPrev.css("display", "none");
+      // }
       $status.text(i + " - " + slick.slideCount);
     }
   );
@@ -24,6 +31,13 @@ $(document).ready(function () {
     focusOnSelect: true,
     infinite: false,
     variableWidth: true,
+    onAfterChange: function (slide, index) {
+      if (index === 4) {
+        $(".slick-next").css("pointer-events", "none");
+      } else {
+        $(".slick-next").css("pointer-events", "all");
+      }
+    },
   });
 
   $(".slider-for").slick({

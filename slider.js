cb89dcadb9d4ea7e28820a45226efcc720e72cad
Julia <yulyakonoval1998@gmail.com>
2021-02-12 18:36:20 +0200

    slide counting view was changed

--- slider.js
+++ slider.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
     function (event, slick, currentSlide, nextSlide) {
       //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
       let i = (currentSlide ? currentSlide : 0) + 1;
-      
-      $status.text(i + "/" + slick.slideCount);
+
+      $status.text(i + " - " + slick.slideCount);
     }
   );
 
